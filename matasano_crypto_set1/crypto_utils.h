4861e4ea0c3a4e40a2fc80d98055825317ede1fe
Evgeni Ron Overchick <Evgeni@evgenis-macbook-air.local>
2016-05-07 01:50:25 +0300

    changed file name in order to represent more accurate use of the function file

---
+++ matasano_crypto_set1/crypto_utils.h
@@ -0,0 +1,20 @@
+//
+//  crypto_utils.h
+//  matasano_crypto_set1
+//
+//  Created by Evgeni Ron Overchick on 4/26/16.
+//  Copyright © 2016 Evgeni Ron Shtrakh. All rights reserved.
+//
+
+#ifndef crypto_utils_h
+#define crypto_utils_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned char BYTE;
+BYTE *hex2base64(const char *str, unsigned int str_len);
+
+
+
+#endif /* crypto_utils_h */
--- matasano_crypto_set1/hex2base64_test.c
+++ matasano_crypto_set1/hex2base64_test.c
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 #include <string.h>
-#include "hex2base64.h"
+#include "crypto_utils.h"
 
 int main(int argc, const char * argv[]) {
     char test_string [] = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
