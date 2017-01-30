37f8c11d8b16b6570833518027eceeaad6545498
Evgeni Ron Overchick <Evgeni@Evgenis-MacBook-Air.local>
2017-01-30 21:37:19 +0200

    Fixed result compare

--- set1_python/set1.py
+++ set1_python/set1.py
@@ -1,5 +1,5 @@
 """Cryptopals Set#1"""
-import sys
+#!/usr/bin/python3
 import binascii
 import base64
 
@@ -9,10 +9,11 @@ def hex2base64(hex_str):
     return base64.b64encode(bin_str)
 
 TEST_STR = (
-    "49276d206b696c6c696e6720796f757220627261696e"
-    "206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    "49276d206b696c6c696e6720796f757220627261696e206c"
+    "696b65206120706f69736f6e6f7573206d757368726f6f6d"
 )
-RESULT = hex2base64(TEST_STR)
+
+RESULT = (hex2base64(TEST_STR)).decode()
 RESULT_COMPARE = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-print RESULT == RESULT_COMPARE
-print sys.version
+print (RESULT == RESULT_COMPARE)
+
